486deb56c5193b2c309551e973ffa87f5969f455
rumus-music <t3guhwahono@gmail.com>
2026-06-08 00:25:01 +0700

    Add WebView functionality to MainActivity
    Implement WebView in MainActivity to load a URL and handle back navigation with ads.

--- app/src/main/java/com/rumusmusic/app/MainActivity.kt
+++ app/src/main/java/com/rumusmusic/app/MainActivity.kt
@@ -1,17 +1,57 @@
 package com.rumusmusic.app
 
-import android.app.Application
-import com.startapp.sdk.adsbase.StartAppSDK
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+import com.startapp.sdk.adsbase.StartAppAd
 
-class MyApplication : Application() {
+class MainActivity : AppCompatActivity() {
 
-    override fun onCreate() {
-        super.onCreate()
+    private lateinit var webView: WebView
+    private var startAppAd: StartAppAd? = null
 
-        StartAppSDK.initParams(
-            applicationContext,
-            "205797420"
-        ).setReturnAdsEnabled(false)
-            .init()
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.settings.loadsImagesAutomatically = true
+        webView.settings.allowFileAccess = true
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl(
+            "https://rumus-music.github.io/"
+        )
+
+        loadInterstitial()
+    }
+
+    private fun loadInterstitial() {
+
+        startAppAd = StartAppAd(this)
+
+        startAppAd?.loadAd()
+    }
+
+    override fun onBackPressed() {
+
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+
+            startAppAd?.showAd()
+
+            super.onBackPressed()
+        }
     }
 }
